8e6545dab56fb2a1502aec0fb234e97dd8bade03
HENRY GEOVANNY ORTEGA MANTILLA <geeovanymantilla@gmail.com>
2020-12-19 08:27:54 -0500

    Inicio del proyecto

---
+++ src/main/java/co/edu/ufps/Examen1151555Application.java
@@ -0,0 +1,13 @@
+package co.edu.ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen1151555Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen1151555Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ufps/Examen1151555ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen1151555ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
